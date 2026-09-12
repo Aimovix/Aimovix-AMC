e06ae88a1fa49c00cb60c88afea6dedc3e71ce53
Aimovix <testen.comnom@gmail.com>
2026-09-12 11:36:08 +0200

    Prepare v1.2.0 APK and ZIP release publishing with private signing

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.agent.mobile"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.1.1"
+        versionCode = 4
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
