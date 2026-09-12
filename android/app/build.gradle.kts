01ae09f1efbcfea1051428aee10b5982e747544b
Teste <testen.comnom@gmail.com>
2026-09-13 00:48:27 +0200

    chore(release): prepare AMC v1.2.2 release with updated APK downloads

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.agent.mobile"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.2.1"
+        versionCode = 6
+        versionName = "1.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
